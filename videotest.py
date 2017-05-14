df98d89e9b58df1c4249fc81d00f54b8d45eb099
Ravi <ravindranath.br@gmail.com>
2017-05-14 22:44:09 +0530

    test all images at once

--- videotest.py
+++ videotest.py
@@ -1,18 +1,27 @@
 import YOLO_small_tf
+from os import listdir
+from os.path import isfile, join
+
 yolo = YOLO_small_tf.YOLO_TF()
 
 images_dir_name = '/var/www/DataScience/Personal/Projects/VideoTask/YOLO_tensorflow/test/video/'
 # filename = 'test/football.jpg'
-filename = images_dir_name + '2.jpg'
 
-#Yolo settings
-yolo.disp_console = True
-# yolo.disp_console = False
-yolo.imshow = False
-yolo.tofile_img = 'test/output/football1.jpg'
-yolo.tofile_txt = 'test/output/football1.txt'
-yolo.filewrite_img = True
-yolo.filewrite_txt = True
+onlyfiles = [f for f in listdir(images_dir_name) if isfile(join(images_dir_name, f))]
 
-yolo.detect_from_file(filename)
+print (onlyfiles)
+for file in onlyfiles:
+    # filename = images_dir_name + '2.jpg'
+    filename = images_dir_name + file
+    #Yolo settings
+    yolo.disp_console = True
+    # yolo.disp_console = False
+    yolo.imshow = False
+    yolo.tofile_img = 'test/output/' + file
+    yolo.tofile_txt = 'test/output/' + file.replace(' ', '')[:-4] + '.txt'
+    yolo.filewrite_img = True
+    yolo.filewrite_txt = True
+
+    yolo.detect_from_file(filename)
+
 # yolo.detect_from_cvmat(cvmat)
